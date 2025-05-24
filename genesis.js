e1bcb6743b125987baefbd0f52db61e0e730aa31
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-05-24 13:20:20 +0100

    Create genesis.js

---
+++ genesis.js
@@ -0,0 +1,16 @@
+echo '{  
+     "config": {  
+       "chainId": 2024,  
+       "homesteadBlock": 0,  
+       "eip150Block": 0,  
+       "eip155Block": 0,  
+       "eip158Block": 0  
+     },  
+     "alloc": {  
+       "0xADMIN_WALLET": { "balance": "50000000000000000000000000" } // 50M MZL  
+     },  
+     "coinbase": "0x0000000000000000000000000000000000000000",  
+     "difficulty": "0x1",  
+     "gasLimit": "0xFFFFFFF",  
+     "timestamp": "0x00"  
+   }' > genesis.json  
